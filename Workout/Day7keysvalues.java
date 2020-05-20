32151edfe7bfa1131a31e9c970224b3da0ab6d02
Saarah0322 <51898956+Saarah0322@users.noreply.github.com>
2020-05-20 21:50:03 +0530

    Add files via upload

--- Workout/Day7keysvalues.java
+++ Workout/Day7keysvalues.java
@@ -7,18 +7,18 @@ public class Day7keysvalues {
 
 	public static void main(String[] args) {
 
-		Map<String, String> map = new HashMap<>();
-        map.put("A", "1");
-        map.put("B", "2");
-        map.put("C", "3");
-        map.put("D", "4");
-        map.put("E", "5");
+		Map<String, Integer> map = new HashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("C", 3);
+        map.put("D", 4);
+        map.put("E", 5);
         
 
         // Get keys and values
-        for (Map.Entry<String, String> entry : map.entrySet()) {
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
             String k = entry.getKey();
-            String v = entry.getValue();
+            Integer v = entry.getValue();
             System.out.println(k + "--> " + v);
         }
   
